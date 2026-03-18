4582d667437c26cef6a32b939fa011a36bb742ea
dsorvq <dsorvq@gmail.com>
2026-03-19 02:03:39 +0300

    Add scanner single token tests

---
+++ tests/scanner_test.cpp
@@ -0,0 +1,52 @@
+#include <gtest/gtest.h>
+
+#include "lox/token/token.hpp"
+#include "lox/scanner/scanner.hpp"
+
+TEST(ScannerTest, SingleTokens) {
+  std::string input = "(){},.-+;/*";
+  std::vector<char> source(input.begin(), input.end());
+  std::span<char> source_span(source);
+	lox::Scanner scanner(source_span);
+  
+  std::vector<lox::Token> tokens = scanner.ScanAll();
+  
+  ASSERT_EQ(tokens.size(), 12);
+  
+  EXPECT_EQ(tokens[0].type_, lox::TokenType::kLeftParent);
+  EXPECT_EQ(tokens[1].type_, lox::TokenType::kRightParent);
+  EXPECT_EQ(tokens[2].type_, lox::TokenType::kLeftBrace);
+  EXPECT_EQ(tokens[3].type_, lox::TokenType::kRightBrace);
+  EXPECT_EQ(tokens[4].type_, lox::TokenType::kComma);
+  EXPECT_EQ(tokens[5].type_, lox::TokenType::kDot);
+  EXPECT_EQ(tokens[6].type_, lox::TokenType::kMinus);
+  EXPECT_EQ(tokens[7].type_, lox::TokenType::kPlus);
+  EXPECT_EQ(tokens[8].type_, lox::TokenType::kSemicolon);
+  EXPECT_EQ(tokens[9].type_, lox::TokenType::kSlash);
+  EXPECT_EQ(tokens[10].type_, lox::TokenType::kStar);
+  EXPECT_EQ(tokens[11].type_, lox::TokenType::kEOF);
+}
+
+TEST(ScannerTest, SingleTokensWithWhitespaces) {
+  std::string input = "( ) { } \t, \n\t\t. - + ; / *";
+  std::vector<char> source(input.begin(), input.end());
+  std::span<char> source_span(source);
+	lox::Scanner scanner(source_span);
+  
+  std::vector<lox::Token> tokens = scanner.ScanAll();
+  
+  ASSERT_EQ(tokens.size(), 12);
+  
+  EXPECT_EQ(tokens[0].type_, lox::TokenType::kLeftParent);
+  EXPECT_EQ(tokens[1].type_, lox::TokenType::kRightParent);
+  EXPECT_EQ(tokens[2].type_, lox::TokenType::kLeftBrace);
+  EXPECT_EQ(tokens[3].type_, lox::TokenType::kRightBrace);
+  EXPECT_EQ(tokens[4].type_, lox::TokenType::kComma);
+  EXPECT_EQ(tokens[5].type_, lox::TokenType::kDot);
+  EXPECT_EQ(tokens[6].type_, lox::TokenType::kMinus);
+  EXPECT_EQ(tokens[7].type_, lox::TokenType::kPlus);
+  EXPECT_EQ(tokens[8].type_, lox::TokenType::kSemicolon);
+  EXPECT_EQ(tokens[9].type_, lox::TokenType::kSlash);
+  EXPECT_EQ(tokens[10].type_, lox::TokenType::kStar);
+  EXPECT_EQ(tokens[11].type_, lox::TokenType::kEOF);
+}
